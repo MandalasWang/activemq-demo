60a37a24b329d55677ef005f1d2060240b366b1a
有缘 <single_violet@163.com>
2019-12-16 20:20:40 +0800

    add:增加配置类

---
+++ src/main/java/ink/boyuan/activemqdemo/config/ActiveMQConfig.java
@@ -0,0 +1,64 @@
+package ink.boyuan.activemqdemo.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+/**
+ * @author wyy
+ * @version 1.0
+ * @date 2019/12/16 20:03
+ * @description
+ **/
+@Component
+public class ActiveMQConfig {
+
+
+    @Value("${spring.activemq.user}")
+    private String usrName;
+
+    @Value("${spring.activemq.password}")
+    private  String password;
+
+    @Value("${spring.activemq.broker-url}")
+    private  String brokerUrl;
+
+    @Bean
+    public Queue queue(){
+        return new ActiveMQQueue("common.topic");
+    }
+
+    @Bean
+    public Topic topic(){
+        return new ActiveMQTopic("common.topic");
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory(usrName, password, brokerUrl);
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerQueue(ActiveMQConnectionFactory connectionFactory){
+        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+        bean.setConnectionFactory(connectionFactory);
+        return bean;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ActiveMQConnectionFactory connectionFactory){
+        DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+        //设置为发布订阅方式, 默认情况下使用的生产消费者方式
+        bean.setPubSubDomain(true);
+        bean.setConnectionFactory(connectionFactory);
+        return bean;
+    }
+}
